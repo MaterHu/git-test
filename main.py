5c0b2ffd4d0d32770fde63c0f5c9dcac294d41f0
MaterHu <1216784717@qq.com>
2025-04-27 18:07:25 +0800

    Revert "greetingBylevel"
    This reverts commit 726a76f7769269963f32433ce526e6e4fb841de2.

--- main.py
+++ main.py
@@ -3,15 +3,9 @@ user = input('请输入姓名：')
 # print(f"hello,{user}")
 
 # 这是一个打招呼测试的业务
-# def greeting(user):
-#     print(f"hello,{user}")
-#
-# greeting(user)
+def greeting(user):
+    print(f"hello,{user}")
 
-def greetingBylevel(user):
-    if user == 'MaterHu':
-        print(f'hello,{user} sama!')
-    else:
-        print(f'hello,dear {user}!')
 
-greetingBylevel(user)
+greeting(user)
+
