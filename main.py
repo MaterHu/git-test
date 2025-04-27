726a76f7769269963f32433ce526e6e4fb841de2
MaterHu <1216784717@qq.com>
2025-04-27 17:57:36 +0800

    greetingBylevel

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ user = input('请输入姓名：')
 # print(f"hello,{user}")
 
 # 这是一个打招呼测试的业务
-def greeting(user):
-    print(f"hello,{user}")
+# def greeting(user):
+#     print(f"hello,{user}")
+#
+# greeting(user)
 
-greeting(user)
+def greetingBylevel(user):
+    if user == 'MaterHu':
+        print(f'hello,{user} sama!')
+    else:
+        print(f'hello,{user}!')
+
+greetingBylevel(user)
